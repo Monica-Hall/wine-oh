587ad67acb3f3b2fe9f72a91eb8cb5c7e9c4dd6f
Monica-Hall <monicakes@Monicas-MacBook-Air.local>
2020-05-21 17:06:01 -0700

    added header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react"; 
+// import "./Header.css"; 
+
+const Header = () => {
+    return (
+        <header className="header">
+            <h1>Wine Oh!</h1>
+            <p>"Some call it a problem,</p>
+            <p>I call it a hobby."</p>
+        </header>
+    )
+}
